5039b68e9049def9672c9ee521ebffb81254ee0a
Mellissa15 <mellissaqoqa@gmail.com>
2026-09-05 16:33:51 +0200

    Add get book by id route

--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { getBooksHandler } from './controllers/books.js';
-console.log('BOOKS ROUTER LOADED');
-
+import {
+    getBooksHandler,
+    getBookByIdHandler
+} from './controllers/books.js';
 
 const router = express.Router();
 
 router.get('/books', getBooksHandler);
+router.get('/books/:id', getBookByIdHandler);
 
 export default router;
+
