25175bf7c1eb3b4832036f33fee417c43e8c3d76
Mellissa15 <mellissaqoqa@gmail.com>
2026-09-10 21:24:52 +0200

    Implement author and book APIs

--- src/models/books.js
+++ src/models/books.js
@@ -14,5 +14,42 @@ const getBookById = async (bookId) => {
     return book;
 };
 
-export { getAllBooks, getBookById };
+const authorExists = async (authorId) => {
+    const db = getDb();
+    const collection = db.collection('authors');
 
+    const author = await collection.findOne({ id: authorId });
+
+    return !!author;
+};
+
+const createBook = async (book) => {
+    const db = getDb();
+    const collection = db.collection('books');
+
+    await collection.insertOne(book);
+
+    return book;
+};
+
+const updateBook = async (id, book) => {
+    const db = getDb();
+    const collection = db.collection('books');
+
+    await collection.updateOne(
+        { id },
+        { $set: book }
+    );
+
+    return { id, ...book };
+};
+
+const deleteBook = async (id) => {
+    const db = getDb();
+    const collection = db.collection('books');
+
+    const result = await collection.deleteOne({ id });
+    return result;
+};
+
+export { getAllBooks, getBookById, authorExists, createBook, updateBook, deleteBook };
